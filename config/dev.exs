bed5363c185d4b9a2bfc065f5b6ac5682d3dd8ac
Justin Morgan <justin@brightbrown.ca>
2020-04-22 18:31:39 -0700

    Configure phoenix_slime for phoenix 1.5 project
    Why:
    
    The phoenix_slime generators do not scaffold files per the new
    Phoenix 1.5 (with the --live flag) format (root, live, app layouts).
    
    * ...
    
    This change addresses the need by:
    - configuring phoenix_slime per documentation
    - manually translate generated layout files to slime

--- config/dev.exs
+++ config/dev.exs
@@ -60,8 +60,8 @@ config :phxslime, PhxslimeWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/phxslime_web/(live|views)/.*(ex)$",
-      ~r"lib/phxslime_web/templates/.*(eex)$"
+      ~r"lib/phxslime_web/(live|views)/.*(ex|leex|slimleex)$",
+      ~r"lib/phxslime_web/templates/.*(eex|slim|slime)$"
     ]
   ]
 
